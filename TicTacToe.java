369ffab3c049734500ff9c76cf108e7430d13626
Ayan-Singh-Tomar <as5438@srmist.edu.in>
2026-05-01 14:32:23 +0530

    UC7: Computer makes a random move

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,93 +1,45 @@
-import java.util.Scanner;
+import java.util.Random;
 
 public class TicTacToe {
 
-    static char[][] board = new char[3][3];
+    static char[][] board = {
+        {'-', '-', '-'},
+        {'-', '-', '-'},
+        {'-', '-', '-'}
+    };
 
-    static Scanner scanner = new Scanner(System.in);
-
-    static char humanSymbol = 'X';
-
+    static char computerSymbol = 'O';
 
     public static void main(String[] args) {
 
-        initializeBoard();
-
-        int slot = getUserSlot();
-
-        int row = getRowFromSlot(slot);
-        int col = getColFromSlot(slot);
-
-        if (isValidMove(row, col)) {
-
-            placeMove(row, col, humanSymbol);
-
-            System.out.println("Move placed successfully");
-
-        } else {
-
-            System.out.println("Invalid move");
-        }
+        computerMove();
 
         printBoard();
     }
 
+    static void computerMove() {
 
+        Random random = new Random();
 
-    static void initializeBoard() {
+        int slot;
+        int row;
+        int col;
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                board[i][j] = '-';
-            }
-        }
+        do {
+
+            slot = random.nextInt(9) + 1;
+
+            row = (slot - 1) / 3;
+            col = (slot - 1) % 3;
+
+        } while (board[row][col] != '-');
+
+        board[row][col] = computerSymbol;
+
+        System.out.println("Computer placed at slot: " + slot);
     }
 
-
-
-    static int getUserSlot() {
-
-        System.out.print("Enter slot number (1-9): ");
-        return scanner.nextInt();
-    }
-
-
-
-    static int getRowFromSlot(int slot) {
-
-        return (slot - 1) / 3;
-    }
-
-
-      static int getColFromSlot(int slot) {
-
-        return (slot - 1) % 3;
-    }
-
-
-
-    static boolean isValidMove(int row, int col) {
-
-        if (row < 0 || row > 2 || col < 0 || col > 2) {
-            return false;
-        }
-
-        if (board[row][col] != '-') {
-            return false;
-        }
-
-        return true;
-    }
-
-
-
-    static void placeMove(int row, int col, char symbol) {
-
-        board[row][col] = symbol;
-    }
-
-
-       static void printBoard() {
+    static void printBoard() {
 
         System.out.println("Current Board:");
 
